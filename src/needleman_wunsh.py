dbc6d3652e6b50384fcf530e9ea86e24565fd359
Jose Cassano <joseluis.cassano@fravega.com.ar>
2021-11-19 18:05:50 -0300

    Fix review final

--- src/needleman_wunsh.py
+++ src/needleman_wunsh.py
@@ -1,10 +1,12 @@
 import numpy as np
 from enum import Enum
 
+
 class Op(Enum):
     GAP_A = 1
     GAP_B = 2
     MA_MM = 3
+
 
 def init(s1, s2):
     score_mtx = init_score_mtx()
@@ -19,6 +21,7 @@ def nw(seqA_str, seqB_str, score_mtx, gap_score=0):
     row = len(seqA_str) + 1
     col = len(seqB_str) + 1
     table = np.repeat(None, row * col).reshape(row, col)
+    tb_table = np.repeat(None, row * col).reshape(row, col)
     """ example:
        [
             _   A   T   C   T
@@ -32,47 +35,54 @@ def nw(seqA_str, seqB_str, score_mtx, gap_score=0):
     table[0][0] = gap_score
 
     for i in range(1, row):
-        table[i][0] = table[i-1][0] + gap_score
+        table[i][0] = table[i - 1][0] + gap_score
+        tb_table[i][0] = Op.GAP_B
 
     for j in range(1, col):
-        table[0][j] = table[0][j-1] + gap_score
+        table[0][j] = table[0][j - 1] + gap_score
+        tb_table[0][j] = Op.GAP_A
 
     for i in range(1, row):
         for j in range(1, col):
-            tmp_match = seqA_str[i-1] + seqB_str[j-1]
+            tmp_match = seqA_str[i - 1] + seqB_str[j - 1]
             ma_mm_score = score_mtx[tmp_match]
-            #TODO: Corregir y agregar otra tabla para hacer traceback
             if ma_mm_score > gap_score:
-                table[i][j] = table[i-1][j-1] + ma_mm_score
+                table[i][j] = table[i - 1][j - 1] + ma_mm_score
+                tb_table[i][j] = Op.MA_MM
             else:
-                gap_en_a = table[i-1][j]
-                gap_en_b = table[i][j-1]
-                table[i][j] = max(gap_en_a, gap_en_b) + gap_score
+                gap_en_b = table[i - 1][j]
+                gap_en_a = table[i][j - 1]
+                if gap_en_b > gap_en_a:
+                    table[i][j] = gap_en_b + gap_score
+                    tb_table[i][j] = Op.GAP_B
+                else:
+                    table[i][j] = gap_en_a + gap_score
+                    tb_table[i][j] = Op.GAP_A
 
-    return [table[row-1, col-1], table]
+    return [table[row - 1, col - 1], tb_table]
+
 
 def traceback(tabla_sol, seqA, seqB):
-    #TODO: Utilizar el enum con otra tabla para hacer el recorrido inverso)
     i = len(seqA)
     j = len(seqB)
 
     aln_a = ""
     aln_b = ""
 
-    while i > 0 and j > 0:
-        ma_mm = tabla_sol[i][j]
-        gap_a = tabla_sol[i-1][j]
-        gap_b = tabla_sol[i][j-1]
-
-        if (ma_mm > gap_a) and (ma_mm > gap_b):
+    while i > 0 or j > 0:
+        last_op = tabla_sol[i][j]
+        # ma_mm = tabla_sol[i-1][j-1]
+        # gap_a = tabla_sol[i - 1][j]
+        # gap_b = tabla_sol[i][j - 1]
+        if last_op == Op.MA_MM:
             aln_a = seqA[i - 1] + aln_a
             aln_b = seqB[j - 1] + aln_b
             i = i - 1
             j = j - 1
-        elif gap_a <= gap_b:
+        elif last_op == Op.GAP_A:
             aln_a = "-" + aln_a
             aln_b = seqB[j - 1] + aln_b
-            j = j-1
+            j = j - 1
         else:
             aln_a = seqA[i - 1] + aln_a
             aln_b = "-" + aln_b
@@ -80,18 +90,11 @@ def traceback(tabla_sol, seqA, seqB):
 
     return [aln_a, aln_b]
 
+
 def init_score_mtx():
-    """ example: MAAAAAL
-    [   A   T   C    T
-       A[1,  0,  0,  0],
-       T[0,  1,  0,  1],
-       A[1,  0,  0,  0]
-    ]
-    """
-    #TODO:
-    # Leer archivo de texto y llevarlo a un diccionario (MAP) en base a la tabla
-    # Combinaciones de letras (AA=5 o AC=-1 o AG=-2, AT=-3, CC=4, CG=-1, etc en cada una)
-    #values = ['A','C','G','T']
+    # TODO:
+    #  Leer archivo de texto y llevarlo a un diccionario (MAP) en base a la tabla
+    # values = ['A','C','G','T']
     score = {
         "AA": 1,
         "CC": 1,
